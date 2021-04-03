41c58b661f85e04ad924107e5e21882627e5a541
Murphy Lee <murphylee2004@gmail.com>
2021-04-02 23:31:38 -0400

    Add method that compares postal codes to CSV codes

--- CustomerSystem.java
+++ CustomerSystem.java
@@ -5,6 +5,8 @@
 
 import java.util.Scanner;
 // More packages may be imported in the space below
+import java.io.File;
+import java.io.FileNotFoundException;
 
 class CustomerSystem{
     public static void main(String[] args){
@@ -105,5 +107,48 @@ class CustomerSystem{
             isValid = false;
         }
         return isValid;
+    }
+
+    /*
+     * Description: Checks if the user-inputted postal code is found in CSV file
+     * 
+     * @author - Murphy Lee
+     * @param sequence - The String that needs to be validated
+     * @throws FileNotFoundException - Exception raised when attempting to read CSV, thrown to ValidatePostalCode method
+     * @return foundInFile - Boolean whose value depends on whether the String is found in the CSV file
+     * */
+    public static boolean compareZip(String postCode) throws FileNotFoundException {
+        String line;  // Will store each line of the file
+        String code;  // Will store postal code from each line
+        boolean foundInFile = false;  // Will be set to true if the code is found in CSV
+        String fileName = "postal_codes.csv";
+
+        // Create File instance of the CSV to read from
+        File text = new File(fileName);
+
+        // Create instances of Scanner
+        Scanner reader = new Scanner(text, "ISO-8859-1");   // Will read text from CSV file in a different character set, to accept accented letters
+        Scanner codeReader;                   // Will store each line from the CSV file
+
+        // Read lines until there are no more
+        while (reader.hasNextLine() == true) {
+            // Read the next line in the file
+            line = reader.nextLine();
+
+            // Store line into seperate scanner - this way we can use the delimeter method
+            codeReader = new Scanner(line);
+
+            // Split line by using the "|" character as a delimiter - since "|" is a special character in Java, we need to escape it by using "\\"
+            codeReader.useDelimiter("\\|");
+            code = codeReader.next();
+            System.out.println(code);
+            // If the user-input matches the postal code, the found variable will becomes true
+            if (postCode.equals(code)) {
+                foundInFile = true;
+            }
+        }
+
+        reader.close();
+        return foundInFile;
     }
 }
